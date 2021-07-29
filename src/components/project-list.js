cbd11790a35f544e284d7d4c83ab737846cb84cb
Mystro Ken <mystroken@gmail.com>
2021-07-29 11:42:00 +0100

    :tada: Add microdata to projects

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import parse from "html-react-parser"
 
 import projects from "../../content/projects.json"
@@ -7,21 +6,20 @@ import projects from "../../content/projects.json"
 export default function ProjectList(props) {
   return (
     <div className="projects" {...props}>
-      {projects.map(({ title, icon, description, link }) => (
+      {projects.map(({ name, icon, description, link }) => (
         <article
           key={link}
           className="project"
           itemScope
-          itemType="https://schema.org/Article"
+          itemType="https://schema.org/SoftwareSourceCode"
         >
           <h3 className="project-title">
-            <a href={link} target="_blank" rel="noreferrer">
-              <div class="icon">📝</div>
-              <h3>TakeNote</h3>
+            <a href={link} target="_blank" rel="noreferrer" itemProp="url">
+              <span class="project-icon">{parse(icon)}</span>
+              <span className="project-name" itemProp="name">
+                {parse(name)}
+              </span>
             </a>
-            <Link to={link} itemProp="url">
-              <span itemProp="headline">{parse(title)}</span>
-            </Link>
           </h3>
           <div className="project-description" itemProp="description">
             {parse(description)}
